a5a13c8fd09e5da039c9f95c04ea089d1165c8d3
Kiran94 <kiran_patel94@hotmail.com>
2015-05-23 00:37:08 +0100

    added template html and custom styles files

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+	<title>Online Fitness Tracker</title>
+
+	<!-- meta -->
+		<meta charset="utf-8" />
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	    <meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!-- end meta -->
+
+	<!-- styles -->
+		<link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="styles/custom.css" />
+	<!-- end styles -->
+</head>
+<body>
+
+
+
+
+
+	<!-- scripts -->
+		<script type="text/javascript" src="scripts/jquery.min.js"></script>
+		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+	<!-- end scripts -->
+</body>
+</html>
