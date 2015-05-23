7844db22bb80768ff204d5830b37787f57b7c05f
Kiran94 <kiran_patel94@hotmail.com>
2015-05-23 12:42:40 +0100

    added connection info file

---
+++ connectionInfo.php
@@ -0,0 +1,11 @@
+<?php
+	
+	$host = "localhost"; 
+	$username = "root"; 
+	$password = ""; 
+	$db = "projects"; 
+
+	error_reporting(E_ERROR | E_PARSE);
+	error_reporting(E_ALL);
+	ini_set('display_errors', 1);
+?>
