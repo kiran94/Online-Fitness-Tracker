06a35a9faf668c4a2d35d6207b8a37fe9a1d3797
Kiran94 <kiran_patel94@hotmail.com>
2015-05-23 18:48:51 +0100

    refactored the request functionality to be query independent, this new class will be used by all functions of the api

--- api/getUser.php
+++ api/getUser.php
@@ -3,21 +3,20 @@
 	//If the user_id value has been set. 
 	if(isset($_GET['user_id']))
 	{
-		//Import the connection code. 
-		require_once "connection.php";
-		//Create a connection object. 
-		$connection = new connection(); 
-		//Create a new connection to the database. 
-		$con = $connection.getConnection(); 
-
 		//Build the query using the parameter
 		$query = "SELECT * FROM user u, measurements m 
 				 WHERE u.user_id =" . $_GET['user_id'] . "AND u.user_id = m.user_id
 				 ORDER BY m.measurement_id DESC
 				 LIMIT BY 1"; 
 
-		//Execute the query. 
-		$result = mysqli_query($con, $query); 
+		//Import code to request data. 
+		require_once "makeRequest.php"; 
+
+		//Create new object for request
+		$request = new makeRequest(); 
+
+		//Make the request and pass the query. 
+		$result = $request.request($query); 
 
 		//For each returned record 
 		while($row = mysqli_fetch_array($result))
@@ -25,9 +24,6 @@
 			//Print values. 
 			echo $row['user_id']; 
 		}
-
-		//Close the connection. 
-		mysqli_close($con); 
 	}
 	else
 	{	
---
+++ api/makeRequest.php
@@ -0,0 +1,25 @@
+<?php
+	//Class provides a means for the application to make requests to the database through one function. 
+	class makeRequest
+	{
+		//Function takes a query and returns a result set. 
+		public function request($query)
+		{
+			//Import the connection code. 
+			require_once "connection.php";
+			//Create a connection object. 
+			$connection = new connection(); 
+			//Create a new connection to the database. 
+			$con = $connection.getConnection(); 
+
+			//Execute the query. 
+			$result = mysqli_query($con, $query); 
+
+			//Close the connection. 
+			mysqli_close($con); 
+
+			//Return the result set. 
+			return $result; 
+		}
+	}
+?>
