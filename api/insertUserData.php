3cfb6d62230e2cb4244e5d795e24d82f297fe99b
Kiran94 <kiran_patel94@hotmail.com>
2015-05-24 10:41:33 +0100

    changed query to retrieve values

--- api/insertUserData.php
+++ api/insertUserData.php
@@ -3,7 +3,7 @@
 	if(isset($_GET['user_name']) || isset($_GET['user_dob']) || isset($_GET['user_height']))
 	{
 		//Build the query using the parameter
-		$query = "INSERT INTO user(user_name, user_dob, user_height) VALUES(" . $_GET['user_name'] ." , " . $_GET['user_dob'] ." , " . $_GET['user_height'] .")"; 
+		$query = "INSERT INTO user(user_name, user_dob, user_height) VALUES('{$_GET['user_name']}' ,'{$_GET['user_dob']}' ,'{$_GET['user_height']}')"; 
 
 		//Import code to request data. 
 		include "makeRequest.php"; 
