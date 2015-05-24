7861da35f5120263cdee60886f6782d1f954935a
Kiran94 <kiran_patel94@hotmail.com>
2015-05-24 11:27:48 +0100

    added comments

--- api/connectionInfo.php
+++ api/connectionInfo.php
@@ -1,9 +1,11 @@
 <?php
+	//Database Connection Credentials. 
 	$host = "localhost"; 
 	$username = "root"; 
 	$password = ""; 
 	$db = "fitnessapp"; 
 
+	//Error Reporting Prefrences. 
 	error_reporting(E_ERROR | E_PARSE);
 	error_reporting(E_ALL);
 	ini_set('display_errors', 1);
