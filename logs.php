83b40a205b167629ed0bf6061c11d50857d6d920
Kiran94 <kiran_patel94@hotmail.com>
2015-05-25 20:37:28 +0100

    started development on logs page

---
+++ logs.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+	<title>Online Fitness Tracker</title>
+
+	<!-- meta -->
+		<meta charset="utf-8" />
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	    <meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!-- end meta -->
+
+	<!-- styles -->
+		<link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="styles/custom.css" />
+	<!-- end styles -->
+</head>
+<body>
+	<!-- container -->
+	<div class="container">
+
+		<!-- Header -->
+		<?php require_once "header.php"; ?>
+		<!-- end header -->
+
+		<!-- navbar -->
+		<div class="row">
+			<div class="col-xs-12">
+				<ul class="nav nav-tabs">
+				  <li role="presentation"><a href="index.php">Home</a></li>
+				  <li role="presentation"><a href="exercise.php">Exercise</a></li>
+				  <li role="presentation" class="active"><a href="#">Logs</a></li>
+				</ul>
+			</div>
+		</div>
+		<!-- end navbar -->
+
+		<!-- body content -->
+		<div class="row">
+			<?php
+				require_once "api/makeRequest.php";
+
+				$req = new makeRequest(); 
+
+				$query = "SELECT e.exercise_name, el.exercise_weight, el.exercise_reps, el.dateVal, e.exercise_img_url
+							FROM user u, exercises e, exerciseLogs el 
+							WHERE u.user_id = el.user_id 
+							AND e.exercise_id = el.exercise_id;"; 
+
+				$result = $req->request($query); 
+
+				while($row = mysqli_fetch_array($result))
+				{
+
+					echo "<div class='col-xs-12 col-sm-10'>";
+						echo "<div class='exercise_nodes'>"; 
+							echo ucfirst($row['exercise_name']) . "<br/>";
+							echo "Weight: " . $row['exercise_weight']. "<br/>"; 
+							echo "Reps: " . $row['exercise_reps']; 
+							echo "Date: " . $row['dateVal'];
+						echo "</div>";
+					echo "</div>"; 
+
+					echo "<div class='col-sm-2'>";
+						echo "<img src=" . $row['exercise_img_url'] . " alt='img' class='img-responsive' />"; 
+					echo "</div>";
+				}
+			?>
+		</div>
+		<!-- end body content -->
+
+		<!-- footer -->
+		<?php require_once "footer.php"; ?>
+		<!-- end footer -->
+
+	</div>
+	<!-- end container -->
+
+	<!-- scripts -->
+		<script type="text/javascript" src="scripts/jquery.min.js"></script>
+		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+	<!-- end scripts -->
+</body>
+</html>
