10a428c031c7e7d64a50f8c88c8c7aeb8fc1219b
Kiran94 <kiran_patel94@hotmail.com>
2015-05-25 13:56:05 +0100

    added exercise page and added some images

---
+++ exercise.php
@@ -0,0 +1,69 @@
+<!DOCTYPE html> 
+<html lang="en">
+<head>
+	<title>Online Fitness Tracker</title>
+
+	<!-- meta -->
+		<meta charset="utf-8" />
+	    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+	    <meta name="viewport" content="width=device-width, initial-scale=1" />
+	<!-- end meta -->
+
+	<!-- styles -->
+		<link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css" />
+		<link rel="stylesheet" type="text/css" href="styles/custom.css" />
+	<!-- end styles -->
+</head>
+<body>
+	<!-- container -->
+	<div class="container">
+
+		<!-- Header -->
+		<?php require_once "header.php"; ?>
+		<!-- end header -->
+
+		<!-- navbar -->
+		<div class="row">
+			<div class="col-xs-12">
+				<ul class="nav nav-tabs">
+				  <li role="presentation"><a href="index.php">Home</a></li>
+				  <li role="presentation" class="active"><a href="#">Exercise</a></li>
+				  <li role="presentation"><a href="logs.php">Logs</a></li>
+				</ul>
+			</div>
+		</div>
+		<!-- end navbar -->
+
+		<!-- body content -->
+		<div class="row">
+
+			<?php
+				require_once "addLogs/getExerciseList.php"; 
+
+				while($row = mysqli_fetch_array($result))
+				{
+					echo "<div class='col-xs-12 col-sm-6'>"; 
+						echo "<h3>" . ucfirst($row['exercise_name']) . "</h3>";
+					echo "</div>"; 
+
+					echo "<div class='col-xs-12 col-sm-5'>";
+						echo "<img src='" . $row['exercise_img_url'] ."' alt='" . $row['exercise_name'] ."' class='img-responsive'/>"; 
+					echo "</div>"; 
+				}
+			?>
+			
+		</div>
+		<!-- end body content -->
+
+		<?php
+			require_once "footer.php";
+		?>
+	</div>
+	<!-- end container -->
+
+	<!-- scripts -->
+		<script type="text/javascript" src="scripts/jquery.min.js"></script>
+		<script type="text/javascript" src="scripts/bootstrap.min.js"></script>
+	<!-- end scripts -->
+</body>
+</html>
