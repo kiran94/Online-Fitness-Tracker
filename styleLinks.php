bd69f93d7dd490d14aadab013c1ffdc7d1826eb4
Kiran94 <kiran_patel94@hotmail.com>
2015-05-26 11:26:07 +0100

    refactored links to a php script

---
+++ styleLinks.php
@@ -0,0 +1,7 @@
+<?php
+	/*
+		CSS File links in central location for all web pages. 
+	*/
+	echo "<link rel='stylesheet' type='text/css' href='styles/bootstrap.min.css'/>";
+	echo "<link rel='stylesheet' type='text/css' href='styles/custom.css' />";
+?>
