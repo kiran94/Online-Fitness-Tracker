d967005cba509a42f0196364f147a3d8c3dd5565
Kiran94 <kiran_patel94@hotmail.com>
2015-05-26 11:31:25 +0100

    added scripts to a central location

---
+++ scriptLinks.php
@@ -0,0 +1,9 @@
+<?php
+
+	/*
+		JS File Links in central location for all web pages. 
+	*/
+	echo "<script type='text/javascript' src='scripts/jquery.min.js'></script>";
+	echo "<script type='text/javascript' src='scripts/bootstrap.min.js'></script>";
+	echo "<script type='text/javascript' src='scripts/submitForm.js'></script>"; 
+?>
