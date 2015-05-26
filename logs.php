1bd17be0cd6e6a13bc573fca454affddeae42886
Kiran94 <kiran_patel94@hotmail.com>
2015-05-26 11:26:43 +0100

    added php script to retrieve style links

--- logs.php
+++ logs.php
@@ -9,9 +9,8 @@
 	    <meta name="viewport" content="width=device-width, initial-scale=1" />
 	<!-- end meta -->
 
-	<!-- styles -->
-		<link rel="stylesheet" type="text/css" href="styles/bootstrap.min.css" />
-		<link rel="stylesheet" type="text/css" href="styles/custom.css" />
+	<!-- styles -->	
+		<?php require_once "styleLinks.php"; ?>
 	<!-- end styles -->
 </head>
 <body>
