b60044a82a0cfdf574f194bbfc0f90767e33850c
Kiran94 <kiran_patel94@hotmail.com>
2015-05-30 10:57:53 +0100

    commented ajax post script

--- scripts/submitForm.js
+++ scripts/submitForm.js
@@ -1,6 +1,6 @@
 $(document).ready(function()
 {	
-
+	//On click of the id element. 
 	$('#addLogsButton').click(function()
 	{
 		//Get Values. 
@@ -9,12 +9,14 @@ $(document).ready(function()
 		var weightVal = $('#weight_option').val(); 
 		var repVal = $('#rep_option').val(); 
 
+		//Get a new date variable. 
 		var dateValue = new Date(); 
+		//Format the date to mysql date variable format. 
 		var formattedDate = dateValue.getUTCFullYear() + "-" + (dateValue.getUTCMonth()+1) + "-" + dateValue.getUTCDate(); 
-		console.log(formattedDate); 
-
+		//Store an object with key/value pairs of the data. 
 		var formData = {exercise_id:exerciseVal, user_id:userVal, exercise_weight:weightVal, exercise_reps:repVal, dateVal:formattedDate};
 
+		//Make an ajax request. 
 		$.ajax(
 		{
 			url: "api/insertLog.php",
@@ -22,18 +24,20 @@ $(document).ready(function()
 			data: formData,
 			success: function(data)
 			{
-				//alert(data); 
-
+				//Parse the returned data into JSON object.
 				var parsed_data = JSON.parse(data); 
-
+				//Get the result data. 
 				var result = parsed_data.Result; 
 
+				//If the post was a success 
 				if(result == "Success")
 				{
+					//Display the success log.
 					$("#alert_log").css("display", "block"); 
 				}
 				else
 				{
+					//Else display the error block. 
 					$("#alert_log_error").css("display", "block");
 				}
 			},
@@ -44,8 +48,6 @@ $(document).ready(function()
 
 		});
 		// end ajax 
-
 	}); 
 	// end button click
-
 });
